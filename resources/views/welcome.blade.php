42ca8157547692ba212c46501a64ce0d7ec41b83
anithaselvaraj002 <anithaselvaraj002@gmail.com>
2022-06-30 15:03:55 +0530

    Commit from VS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Anitha
+                    Laravel project
                 </div>
 
                 <div class="links">
